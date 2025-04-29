ba1a07ca7af016d0ad8e09ec83c53f50777e3853
Roberto B F Filho <90075190+Roberto180@users.noreply.github.com>
2025-04-29 00:59:17 -0300

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -32,3 +32,19 @@ function nextImage(){
 
 document.getElementById("radio"+count).checked = true;
 }
+
+const navLinks = document.querySelectorAll('.nav-list a');
+
+window.addEventListener('scroll', () => {
+    navLinks.forEach(link => {
+        const section = document.querySelector(link.getAttribute('href'));
+        const sectionTop = section.offsetTop - 50; 
+        const sectionHeight = section.offsetHeight;
+
+        if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+            link.classList.add('active'); 
+        } else {
+            link.classList.remove('active'); 
+        }
+    });
+});
